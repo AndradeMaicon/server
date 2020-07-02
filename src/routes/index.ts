f637d9b3ede80a117ed6525f6fbd42f17534e492
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-07-01 21:33:17 -0300

    Route: Item listing created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import knex from '../database/connection'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({ Message: 'E la vamos nos' })
+router.get('/items', async (req, res) => {
+  const items = await knex('items').select('*')
+
+  const serealizedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  })
+
+  return res.json(serealizedItems)
 })
 
 export default router
