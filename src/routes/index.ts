f2e15a2ac344ca0782bec91fb7fb37dea5932030
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-07-01 22:15:39 -0300

    Route: create points

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,12 +8,50 @@ router.get('/items', async (req, res) => {
 
   const serealizedItems = items.map(item => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`
     }
   })
 
   return res.json(serealizedItems)
+})
+
+router.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longetude,
+    city,
+    uf,
+    items
+  } = req.body
+
+  const insertedIds = await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longetude,
+    city,
+    uf
+  })
+
+  const point_id = insertedIds[0]
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id
+    }
+  })
+
+  await knex('point_items').insert(pointItems)
+
+  return res.json({ success: true })
 })
 
 export default router
